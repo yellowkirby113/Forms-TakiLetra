761127b042e62b3fdb3a24fcdea13a273c4928f0
yellowkirby111 <a25633@aepombal.edu.pt>
2025-12-02 11:09:06 +0000

    +Book Control.cs e [disigner]

---
+++ Controls/books.Designer.cs
@@ -0,0 +1,50 @@
+namespace Forms_TakiLetra.Controls
+{
+    partial class books
+    {
+        /// <summary>Required designer variable.</summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>Clean up any resources being used.</summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private System.Windows.Forms.Label lblTitle;
+
+        #region Component Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(16, 16);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Livros";
+            // 
+            // books
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.lblTitle);
+            this.Name = "books";
+            this.Size = new System.Drawing.Size(800, 450);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+    }
+}
---
+++ Controls/books.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace Forms_TakiLetra.Controls
+{
+    public partial class books : UserControl
+    {
+        public books()
+        {
+            InitializeComponent();
+        }
+    }
+}
